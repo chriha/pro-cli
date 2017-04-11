f0408b80ef1824de829177824dc99912c5ab4149
Christopher Hartmann <dev@chriha.de>
2017-04-11 12:53:40 +0200

    add spinner function

--- functions.sh
+++ functions.sh
@@ -142,3 +142,33 @@ filemtime() {
 
     echo $TIMESTAMP
 }
+
+
+# # # # # # # # # # # # # # # # # # # #
+# spinner
+# (sleep 4) &
+# spinner $1 "A nice text ..."
+spinner() {
+    local cl="\r\033[K"
+    local pid=$1
+    local spinnging=true
+    local delay=0.05
+    local spinstr="⠏⠛⠹⠼⠶⠧"
+
+    printf "  "
+
+    while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
+        local tmp=${spinstr#?}
+
+        if [ -z "$2" ]; then
+            printf " \b\b\b${tmp:0:1} "
+        else
+            printf "${cl} ${tmp:0:1} ${2}"
+        fi
+
+        local spinstr=$tmp${spinstr%"$tmp"}
+        sleep $delay
+    done
+
+    printf "${cl}"
+}
