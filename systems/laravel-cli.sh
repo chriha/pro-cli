8ac6ef57cbd8207ed03903d5fc5e4403d2d217fb
Christopher Hartmann <dev@chriha.de>
2017-09-06 10:16:59 +0200

    correct permissions

--- systems/laravel-cli.sh
+++ systems/laravel-cli.sh
@@ -4,12 +4,12 @@
 # execute artisan commands
 if [ "$1" == "artisan" ]; then
     shift
-    $RUN web php artisan $@
+    $RUN web php artisan $@ 2> $OUTPUT_FILE
     exit
 
 # # # # # # # # # # # # # # # # # # # #
 # interact with the application
 elif [ "$1" == "tinker" ]; then
-    $RUN web php artisan tinker
+    $RUN web php artisan tinker 2> $OUTPUT_FILE
     exit
 fi
