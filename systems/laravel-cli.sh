b3b24348b7857b3dc72642502c0a436bf3c4caa7
Christopher Hartmann <dev@chriha.de>
2018-01-19 01:02:27 +0100

    update laravel env file, commands, docker-compose file

--- systems/laravel-cli.sh
+++ systems/laravel-cli.sh
@@ -17,4 +17,10 @@ if [ "$1" == "artisan" ]; then
 elif [ "$1" == "tinker" ]; then
     $RUN $PC_USER_PARAM web php artisan tinker 2> $OUTPUT_FILE
     exit
+
+# # # # # # # # # # # # # # # # # # # #
+# execute laravel-echo-server commands
+elif [ "$1" == "echo" ]; then
+    $RUN $PC_USER_PARAM echo laravel-echo-server 2> $OUTPUT_FILE
+    exit
 fi
