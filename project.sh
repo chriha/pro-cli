23ef4f2fddd20025d581f20a45a08103c8fbe418
Christopher Hartmann <dev@chriha.de>
2018-03-11 03:58:27 +0100

    minor changes to update process

--- project.sh
+++ project.sh
@@ -7,7 +7,7 @@ PC_DIR="$HOME/.pro-cli"
 reset_output
 
 if ! jq --version &> /dev/null; then
-    printf "${RED}jq not installed, but is requirement.${NORMAL}\n"
+    printf "${RED}jq not installed, but is required.${NORMAL}\n"
     exit
 fi
 
